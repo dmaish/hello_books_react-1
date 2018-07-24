31d06a34ceb0b95c18df0c447d777c8238735264
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2018-07-24 19:31:10 +0300

    [Fix] Tests errors

--- src/tests/break.test.js
+++ src/tests/break.test.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import BreakLine from "../components/common/break";
 
 it("renders withput crashing", () => {
-	const div = document.createElement("div");
+	const div = div;
 	ReactDOM.render(<BreakLine/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
