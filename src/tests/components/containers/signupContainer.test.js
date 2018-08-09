ec9b7a13bf3261557ad8cd7dc06479c7dd07e1bb
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2018-08-09 13:57:37 +0300

    [Feature #159197750] Fix signup test

--- src/tests/components/containers/signupContainer.test.js
+++ src/tests/components/containers/signupContainer.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {shallow} from "enzyme";
 import store from "../../../store";
-import SignUpContainer from "../../../components/containers/signUpContainer";
+import SignUpContainer from "../../../components/containers/signupContainer";
 
 it("renders without crashing", () => {
   shallow(<SignUpContainer store={store}/>)
