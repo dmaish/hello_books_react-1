e7913ce9df5e1c89a2935d35ee3e26f3c780e9ab
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2018-08-14 00:04:11 +0300

    [Feature #159746194] Get featured books

--- src/tests/components/home/featured.test.js
+++ src/tests/components/home/featured.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RecentlyAdded from "../../../components/home/recentlyAdded";
+import store from "../../../store";
+import Featured from "../../../components/home/featured";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<RecentlyAdded />, div);
+  ReactDOM.render(<Featured store={store}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
